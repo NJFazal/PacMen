20ef584a4a2d75a4e6e96e23863ddc5d225b047c
Nicole Fazal <108958417+NJFazal@users.noreply.github.com>
2022-09-26 21:36:23 -0400

    Update pacmen.js

--- pacmen.js
+++ pacmen.js
@@ -1,13 +1,12 @@
 
 <script>
-let pos = 0;
+var pos = 0;
 const pacArray = [
-  ['PacMan1.png', 'PacMan2.png'],
-  ['PacMan3.png', 'PacMan4.png'],
+  ['./images/PacMan1.png', './images/PacMan2.png'],
+  ['./images/PacMan3.png', './images/PacMan4.png'],
 ];
-let direction = 0;
-const pacMen = []; 
-
+var direction = 0;
+const pacMen = [];
 
 function setToRandom(scale) {
   return {
@@ -16,26 +15,20 @@ function setToRandom(scale) {
   };
 }
 
-
 function makePac() {
-  
-  let velocity = setToRandom(10); // {x:?, y:?}
-  let position = setToRandom(200);
-
  
+  let velocity = setToRandom(10);
+  let position = setToRandom(200);
+  
   let game = document.getElementById('game');
   let newimg = document.createElement('img');
   newimg.style.position = 'absolute';
-  newimg.src = 'PacMan1.png';
+  newimg.src = './images/PacMan1.png';
   newimg.width = 100;
   newimg.style.left = position.x;
   newimg.style.top = position.y;
-  
-
- 
   game.appendChild(newimg);
 
- 
   return {
     position,
     velocity,
@@ -57,9 +50,16 @@ function update() {
 }
 
 function checkCollisions(item) {
-  
-  if (item.position.x + item.velocity.x + item.newimg.width > window.innerWidth || item.position.x + item.velocity.x < 0) item.velocity.x = -item.velocity.x;
-  if (item.position.y + item.velocity.y + item.newimg.height > window.innerHeight || item.position.y + item.velocity.y < 0) item.velocity.y = -item.velocity.y;
+  if (
+    item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
+    item.position.x + item.velocity.x < 0
+  )
+    item.velocity.x = -item.velocity.x;
+  if (
+    item.position.y + item.velocity.y + item.newimg.height > window.innerHeight ||
+    item.position.y + item.velocity.y < 0
+  )
+    item.velocity.y = -item.velocity.y;
 }
 
 function makeOne() {
@@ -70,4 +70,5 @@ function makeOne() {
 if (typeof module !== 'undefined') {
   module.exports = { checkCollisions, update, pacMen };
 }
+
 </script>
